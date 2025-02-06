7b303b0f3732d0637469802b4e81444aed367ade
Nikita-CHetvertukhin <lardgreat@gmail.com>
2025-02-06 19:21:28 +0100

    alpha 0.1.10
    clickAllButtons

--- src/test/java/SmokeTestCase/T4_menu_Work_Area.java
+++ src/test/java/SmokeTestCase/T4_menu_Work_Area.java
@@ -182,91 +182,103 @@ public class T4_menu_Work_Area {
 	    @Test(groups = {"smoke", "speed"})
 
 	    public void t4_4_Header_buttons() {
-	        System.out.println("Запуск t4_4_Header_buttons");
+			
+			System.out.println("Запуск t4_4_Header_buttons");
 	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	        WebDriverWait shortWait = new WebDriverWait(driver, Duration.ofSeconds(1));
 
 	        // Основной цикл для прохождения по всем кнопкам
-	        List<WebElement> buttons = driver.findElements(By.xpath("//div[@class='header-menu']//a[@class='btn default']"));
+	        List<WebElement> buttons = driver.findElements(By.xpath("//div[@class='header-menu']//a[@class='btn default'] | //div[@class='header-menu']//div[@class='btn']"));
+	        System.out.println("buttons.size() равен: " + (buttons.size()));
+	        
 	        for (int i = 0; i < buttons.size(); i++) {
-	            WebElement button = buttons.get(i);
+	        	WebElement button = buttons.get(i);
+	        	System.out.println("Текущий номер кнопки: " + (i));
 	            wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+	            
+	        	List<WebElement> scrollerItems = button.findElements(By.xpath(".//div[@class='scroller items']"));
+		            if (!scrollerItems.isEmpty()) {
+		            	//List<WebElement> tableRows = scrollerItems.findElements(By.xpath(".//div[@class='cell']"));
+		            	List<WebElement> tableRows = new ArrayList<>();
+		            	for (WebElement scrollerItem : scrollerItems) {
+		            	    List<WebElement> cells = scrollerItem.findElements(By.xpath(".//div[@class='cell']"));
+		            	    tableRows.addAll(cells);
+		            	}
+		            	System.out.println("tableRows.size() равен: " + (tableRows.size()));
+		            	
+		            	for (int k = 0; k < tableRows.size(); k++) {
+		            		WebElement row = tableRows.get(k);
+		            		System.out.println("Текущий номер строки: " + (k));
+		            		hoverOverElement(row);// наведение курсора на строку row первого уровня button
+		            		timing();
+		            		List<WebElement> secondScrollerItems = row.findElements(By.xpath(".//div[contains(@class, 'cell')]"));
+		                    if (!secondScrollerItems.isEmpty()) { //Определение есть ли подсписок
+		                    	System.out.println("submenu found: ");
+		                    	System.out.println("Количество элементов: " + secondScrollerItems.size());
+		                    	// Вывод названий всех элементов подсписка
+		                        for (WebElement item : secondScrollerItems) {
+		                            System.out.println("Название элемента: " + item.getText());
+		                        }
+		                    	
+		                    	for (int n = 0; n < secondScrollerItems.size(); n++) {
+		    	            		WebElement item = secondScrollerItems.get(n);
+		    	            		System.out.println("Текущий номер элемента: " + (n));
+		                    		wait.until(ExpectedConditions.elementToBeClickable(item)).click();
+		                    		CloseButton(shortWait);
+		                    		
+		                    		// Перезагрузить список кнопок перед повторным открытием выпадающего списка
+		                    		//buttons = driver.findElements(By.xpath("//div[@class='header-menu']//a[@class='btn default'] | //div[@class='header-menu']//div[@class='btn']"));
+				                    button = buttons.get(i);
+				                    wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+				                    
+				                 // Перезагрузить список первого уровня перед повторным открытием выпадающего списка
+				                    //tableRows = driver.findElements(By.xpath("//tr[@class='item menu' or contains(@class, 'item menu active')]"));
+				                    row = tableRows.get(k);
+				                    hoverOverElement(row);// наведение курсора на строку row первого уровня button
+				                    timing();
+		                    	}
+		                    	
+		                    	//Перезагрузить страницу
+		                    	   
+		                    } else { //Если нет подсписка
+		                    	System.out.println("submenu not found");
+		                        wait.until(ExpectedConditions.elementToBeClickable(row)).click();
+		                        CloseButton(shortWait); //После клика на row если появляется мешающие всплывающие окна - убираем их
+		                        
+		                     // Перезагрузить список кнопок перед повторным открытием выпадающего списка
+		                        buttons = driver.findElements(By.xpath("//div[@class='header-menu']//a[@class='btn default'] | //div[@class='header-menu']//div[@class='btn']"));
+			                    button = buttons.get(i);
+			                    wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+		                    }
+		            	}
+		                    
+		            } else {
+		            	//здесь действия если у кнопок в header-menu нет выпадашки
+		            }
+		            
+		            //Перезагрузить страницу после обработки каждой кнопки (из-за перестраивания DOM)
+		            driver.navigate().refresh();
+		            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='header-menu']//a[@class='btn default'] | //div[@class='header-menu']//div[@class='btn']")));
 
-	            List<WebElement> scrollerItems = driver.findElements(By.xpath("//div[@class='scroller items']"));
-	            if (!scrollerItems.isEmpty()) {
-	                List<WebElement> tableRows = driver.findElements(By.xpath("//tr[@class='item menu' or contains(@class, 'item menu active')]"));
-	                for (WebElement row : tableRows) {
-	                	hoverOverElement(row);
-	                    if (additionalDropdownAppears()) {
-	                    	System.out.println("additionalDropdownAppears: true");
-	                    	// Добавьте здесь логику для обработки выпадающего списка
-	                    	
-	                    } else {
-	                    	System.out.println("additionalDropdownAppears: false");
-	                        wait.until(ExpectedConditions.elementToBeClickable(row)).click();
-	                        handleRowActions(shortWait, row);
-	                    }
-
-	                    // Перезагрузить список кнопок перед повторным открытием выпадающего списка
-	                    buttons = driver.findElements(By.xpath("//div[@class='header-menu']//a[@class='btn default']"));
-	                    button = buttons.get(i);
-	                    wait.until(ExpectedConditions.elementToBeClickable(button)).click();
-	                }
-	            } else {
-	                System.out.println("Element <div class='scroller items'> not found.");
-	            }
-
-	            // Перезагрузить страницу после обработки каждой кнопки (из-за перестраивания DOM)
-	            driver.navigate().refresh();
-	            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='header-menu']//a[@class='btn default']")));
-
-	            // Обновить список кнопок после перезагрузки страницы
-	            buttons = driver.findElements(By.xpath("//div[@class='header-menu']//a[@class='btn default']"));
+		            // Обновить список кнопок после перезагрузки страницы
+		            buttons = driver.findElements(By.xpath("//div[@class='header-menu']//a[@class='btn default'] | //div[@class='header-menu']//div[@class='btn']"));
 	        }
-	        System.out.println("Вывод в консоль вообще-то работает");
+		}
+		//метод наведения на строку выпадающего списка
+	    private void hoverOverElement(WebElement element) {
+	        Actions actions = new Actions(driver);
+	        actions.moveToElement(element).perform();
 	    }
-	    
 	    //Перманентная задержка между кликами в 1 секунду (из-за перестраивания DOM)
-	    private void handleRowActions(WebDriverWait shortWait, WebElement row) {
+	    private void timing() {
 	        try {
 	            Thread.sleep(1000); // Задержка в 1 секунду
 	        } catch (InterruptedException e) {
 	            e.printStackTrace();
 	        }
-
-	        handleCloseButton(shortWait);
-	        handleTableRow(row);
 	    }
-	    
-	    //метод наведения на строку выпадающего списка
-	    private void hoverOverElement(WebElement element) {
-	        Actions actions = new Actions(driver);
-	        actions.moveToElement(element).perform();
-	    }
-	    
-	    //Характеристики классов выпадающего списка второго уровня...
-	    private boolean additionalDropdownAppears() {
-	        try {
-	            // Добавим небольшую задержку перед проверкой наличия выпадающего списка
-	            Thread.sleep(1000); // Секунда задержки
-
-	            WebElement dropdown = driver.findElement(By.xpath("//div[contains(@class, 'submenu')]"));
-	            boolean isDisplayed = dropdown.isDisplayed();
-	            System.out.println("submenu found: " + isDisplayed);
-	            return isDisplayed;
-	        } catch (NoSuchElementException e) {
-	            System.out.println("submenu not found");
-	            return false;
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	            return false;
-	        }
-	    }
-
-
-	    
-	    //Метод закрытия мешающих экранов в процессе прокликивания кнопок
-	    private void handleCloseButton(WebDriverWait shortWait) {
+	  //Метод закрытия мешающих экранов в процессе прокликивания кнопок
+	    private void CloseButton(WebDriverWait shortWait) {
 	        try {
 	            WebElement closeButton = shortWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//a[@class='btn-tool btn default no-text' and descendant::i[@class='fa-close fa icon']]")));
 	            if (closeButton != null) {
@@ -277,30 +289,4 @@ public class T4_menu_Work_Area {
 	            System.out.println("Кнопка 'Закрыть' не найдена.");
 	        }
 	    }
-	    
-	    //метод для обработки строк выпадающего списка первого уровня
-	    private void handleTableRow(WebElement row) {
-	        List<WebElement> cellDivs = row.findElements(By.xpath(".//div[contains(@class, 'cell')]"));
-	        if (!cellDivs.isEmpty()) {
-	            WebElement cellDiv = cellDivs.get(0);
-	            List<WebElement> uniqueTextDivs = cellDiv.findElements(By.xpath(".//div[contains(@class, ' text') and @title]"));
-	            if (!uniqueTextDivs.isEmpty()) {
-	                WebElement uniqueTextDiv = uniqueTextDivs.get(0);
-	                String titleAttribute = uniqueTextDiv.getDomAttribute("title");
-	                System.out.println("Title attribute: " + titleAttribute);
-
-	                String rowClass = row.getDomAttribute("class");
-	                if (rowClass.contains("active")) {
-	                    System.out.println("Row is active: " + titleAttribute);
-	                } else {
-	                    System.out.println("Row is not active: " + titleAttribute);
-	                }
-	            } else {
-	                System.out.println("Element <div class='text' and @title> not found inside <div class='cell'>.");
-	            }
-	        } else {
-	            System.out.println("Element <div class='cell'> not found inside row.");
-	        }
-	    }
-
 }
